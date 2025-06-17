6b8587cffc12759a600132c7199f7c6e81a63a9d
DevSadit <dev.sadit@gmail.com>
2025-06-17 14:28:45 +0600

    authorization with github completed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-
 const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
-  return <div>
-    <Navbar/>
-    {children}</div>;
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
 };
 
 export default layout;
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import { auth, signOut, signIn } from "@/auth";
+import { redirect } from "next/dist/server/api-utils";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
+
   return (
-    <div> Navbar</div>
-  )
-}
+    <header className="bg-white shadow-md px-5 py-3">
+      <nav className="flex items-center justify-between">
+        <Link href={"/"}>
+          <Image src={"/logo.png"} alt="Logo" width={140} height={30} />
+        </Link>
 
-export default Navbar
+        <div className="flex items-center gap-5 text-black ">
+          {session && session.user ? (
+            <>
+              <Link href={"startup/create"}>
+                <span>Create</span>
+              </Link>
+
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" }); 
+                }}
+              >
+                <button type="submit">
+                  <span>Logout</span>
+                </button>
+              </form>
+
+              <Link href={`/user/${session.user?.id}`}>
+                <span>{session.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button type="submit">
+                <span>LogIn</span>
+              </button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
